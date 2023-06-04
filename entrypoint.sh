9fafb305098800b26c4268961e5c8fb5341cc13d
sasymj <124588564+sasymj@users.noreply.github.com>
2023-06-04 12:46:01 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='e4e2d30f-3c66-4fd8-9d41-41e83076f086'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
